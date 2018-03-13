ca903d7289ff776b9c549eccc24b1df114efa81b
lukmis <32072210+lukmis@users.noreply.github.com>
2018-03-13 13:38:58 -0700

    Use container exit code to exit Trainer process. (#9)

--- src/container_support/training.py
+++ src/container_support/training.py
@@ -1,11 +1,19 @@
+import container_support as cs
+import logging
+import os
 import traceback
 from container_support import TrainingEnvironment
+
+logger = logging.getLogger(__name__)
 
 
 class Trainer(object):
     @classmethod
     def start(cls):
         base_dir = None
+        exit_code = 0
+        cs.configure_logging()
+        logger.info("Training starting")
         try:
             env = TrainingEnvironment()
             env.start_metrics_if_enabled()
@@ -19,5 +27,11 @@ class Trainer(object):
         except Exception as e:
             trc = traceback.format_exc()
             message = 'uncaught exception during training: {}\n{}\n'.format(e, trc)
+            logger.error(message)
             TrainingEnvironment.write_failure_file(message, base_dir)
+            exit_code = 1 if not hasattr(e, 'errno') else e.errno
             raise e
+        finally:
+            # Since threads in Python cannot be stopped, this is the only way to stop the application
+            # https://stackoverflow.com/questions/9591350/what-is-difference-between-sys-exit0-and-os-exit0
+            os._exit(exit_code)
