4cbc270c8039effe40244173b308bad4118e6b29
Marcio Vinicius dos Santos <mvs.usp@gmail.com>
2018-04-13 10:36:16 -0700

    Add configuration files (#24)
    * Add configuration files

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import find_packages, setup
+
+
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+
+setup(
+    name='sagemaker_container_support',
+    version='2.0.0',
+    description='Open source library for creating containers to run on Amazon SageMaker.',
+
+    packages=find_packages(where='src', exclude=('test',)),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    long_description=read('README.md'),
+    author='Amazon Web Services',
+    url='https://github.com/aws/sagemaker-container-support/',
+    license='Apache License 2.0',
+
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
+    extras_require={
+        'test': ['tox', 'flake8', 'pytest', 'pytest-cov']
+    }
+)
---
+++ test/test_no_tests_implemented.py
@@ -0,0 +1,2 @@
+def test_no_tests_implemented():
+    assert True
