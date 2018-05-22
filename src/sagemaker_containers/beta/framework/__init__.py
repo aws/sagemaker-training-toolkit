b1ee471eddd1cace09ca06c074da031faf54b9f2
Marcio Dos Santos <mvs@amazon.com>
2018-05-21 22:42:26 -0700

    Beta message

--- src/sagemaker_containers/beta/framework/__init__.py
+++ src/sagemaker_containers/beta/framework/__init__.py
@@ -10,6 +10,7 @@
 # distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+"""BETA module containing volatile or experimental code."""
 from __future__ import absolute_import
 
 # flake8: noqa ignore=F401 imported but unused
