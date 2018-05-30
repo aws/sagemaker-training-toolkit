c925ac74b91717bb4c9fceabc6703af28e391121
Marcio Dos Santos <mvs@amazon.com>
2018-05-30 13:50:54 -0700

    Fix setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ packages.append('sagemaker_containers.etc')
 
 setup(
     name='sagemaker_containers',
-    version='2.0.1',
+    version='2.0.2',
     description='Open source library for creating containers to run on Amazon SageMaker.',
 
     packages=packages,
