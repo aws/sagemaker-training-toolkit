b4ea5da26a0e71b2b79446f517f998c3b276b2ae
Ajay Karpur <akarpur@amazon.com>
2020-02-10 14:39:33 -0800

    fix: change scipy version pin to lower bound (#24)
    * fix: Change scipy pin to lower bound
    
    * Address pylint violations with temporary placeholders
    
    * Update docstrings to address pylint violation

--- src/sagemaker_containers/_mapping.py
+++ src/sagemaker_containers/_mapping.py
@@ -165,16 +165,20 @@ class MappingMixin(collections.Mapping):
         return isinstance(getattr(type(self), _property), property)
 
     def __getitem__(self, k):
+        """Built-in method override"""
         if not self._is_property(k):
             raise KeyError("Trying to access non property %s" % k)
         return getattr(self, k)
 
     def __len__(self):
+        """Built-in method override"""
         return len(self.properties())
 
     def __iter__(self):
+        """Built-in method override"""
         items = {_property: getattr(self, _property) for _property in self.properties()}
         return iter(items)
 
     def __str__(self):
+        """Built-in method override"""
         return str(dict(self))
