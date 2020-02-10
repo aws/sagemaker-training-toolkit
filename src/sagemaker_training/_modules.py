85d47b0d63d797ba1d93796aeb5d644973c142e8
Ajay Karpur <akarpur@amazon.com>
2020-02-10 14:58:24 -0800

    fix: modify download_and_install to work with local tarball (#25)
    * Address pylint violations with temporary placeholders
    
    * Remove redundant call to reload_module
    
    * fix: Modify download_and_install to work with local tarball
    
    * Address flake8 violation
    
    * Update docstrings to address pylint violation

--- src/sagemaker_training/_modules.py
+++ src/sagemaker_training/_modules.py
@@ -18,7 +18,6 @@ import os
 import shlex
 import subprocess  # pylint: disable=unused-import
 import sys
-import tarfile
 import textwrap
 import warnings
 
@@ -156,18 +155,10 @@ def download_and_install(uri, name=DEFAULT_MODULE_NAME, cache=True):
 
     if not should_use_cache:
         with _files.tmpdir() as tmpdir:
-            if uri.startswith("s3://"):
-                dst = os.path.join(tmpdir, "tar_file")
-                _files.s3_download(uri, dst)
-                module_path = os.path.join(tmpdir, "module_dir")
-                os.makedirs(module_path)
-
-                with tarfile.open(name=dst, mode="r:gz") as t:
-                    t.extractall(path=module_path)
-
-            else:
-                module_path = uri
-
+            dst = os.path.join(tmpdir, "tar_file")
+            _files.download_and_extract(uri, dst)
+            module_path = os.path.join(tmpdir, "module_dir")
+            os.makedirs(module_path)
             prepare(module_path, name)
             install(module_path)
 
