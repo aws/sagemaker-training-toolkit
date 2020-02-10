e8e1bc81650e9a07cad6068d44fb85e0e29ced62
Ajay Karpur <akarpur@amazon.com>
2020-02-10 11:06:28 -0800

    Address pylint violations with temporary placeholders

--- src/sagemaker_containers/_mapping.py
+++ src/sagemaker_containers/_mapping.py
@@ -165,16 +165,20 @@ class MappingMixin(collections.Mapping):
         return isinstance(getattr(type(self), _property), property)
 
     def __getitem__(self, k):
+        """Placeholder docstring"""
         if not self._is_property(k):
             raise KeyError("Trying to access non property %s" % k)
         return getattr(self, k)
 
     def __len__(self):
+        """Placeholder docstring"""
         return len(self.properties())
 
     def __iter__(self):
+        """Placeholder docstring"""
         items = {_property: getattr(self, _property) for _property in self.properties()}
         return iter(items)
 
     def __str__(self):
+        """Placeholder docstring"""
         return str(dict(self))
