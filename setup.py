6593f1d51556333bed1d862a49cf95a2e60db3cd
Ajay Karpur <akarpur@amazon.com>
2020-02-14 15:22:56 -0800

    Format with black

--- setup.py
+++ setup.py
@@ -90,9 +90,5 @@ setuptools.setup(
             "black==19.3b0 ; python_version >= '3.6'",
         ]
     },
-    entry_points={
-        "console_scripts": [
-            "train=sagemaker_training.cli.train:main",
-        ]
-    },
+    entry_points={"console_scripts": ["train=sagemaker_training.cli.train:main"]},
 )
