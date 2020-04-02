a25968bc53a3a95e0bdc527e159825e4c6cb99cd
Ajay Karpur <akarpur@amazon.com>
2020-04-02 14:22:27 -0700

    fix: Extract module to correct location in download_and_install (#40)
    Co-authored-by: Lauren Yu <6631887+laurenyu@users.noreply.github.com>

--- test/unit/test_modules.py
+++ test/unit/test_modules.py
@@ -214,17 +214,31 @@ def test_import_module(reload, import_module, install, download_and_extract):
     reload.assert_called_with(import_module(modules.DEFAULT_MODULE_NAME))
 
 
-def test_download_and_install_local_directory():
+@patch("sagemaker_training.modules.exists", return_value=False)
+@patch("sagemaker_training.files.tmpdir")
+@patch("sagemaker_training.files.download_and_extract")
+@patch("sagemaker_training.modules.prepare")
+@patch("sagemaker_training.modules.install")
+def test_download_and_install(install, prepare, download_and_extract, files_tmpdir, module_exists):
+    files_tmpdir.return_value.__enter__.return_value = "tmp"
+    uri = "s3://foo/bar"
+    modules.download_and_install(uri)
+
+    module_path = os.path.join("tmp", "module_dir")
+    download_and_extract.assert_called_with(uri, module_path)
+    prepare.assert_called_with(module_path, "default_user_module_name")
+    install.assert_called_with(module_path)
+
+
+@patch("sagemaker_training.files.s3_download")
+@patch("tarfile.open")
+@patch("sagemaker_training.modules.prepare")
+@patch("sagemaker_training.modules.install")
+def test_download_and_install_local_directory(install, prepare, tarfile, s3_download):
     uri = "/opt/ml/input/data/code/sourcedir.tar.gz"
+    modules.download_and_install(uri)
 
-    with patch("sagemaker_training.files.s3_download") as s3_download, patch(
-        "tarfile.open"
-    ) as tarfile, patch("sagemaker_training.modules.prepare") as prepare, patch(
-        "sagemaker_training.modules.install"
-    ) as install:
-        modules.download_and_install(uri)
-
-        s3_download.assert_not_called()
-        tarfile.assert_called_with(name="/opt/ml/input/data/code/sourcedir.tar.gz", mode="r:gz")
-        prepare.assert_called_once()
-        install.assert_called_once()
+    s3_download.assert_not_called()
+    tarfile.assert_called_with(name="/opt/ml/input/data/code/sourcedir.tar.gz", mode="r:gz")
+    prepare.assert_called_once()
+    install.assert_called_once()
