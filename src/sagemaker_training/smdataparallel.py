e4d5a173b21344778435f3fb8ff5fce5d31999c6
Dan <choidan@amazon.com>
2020-11-18 07:20:16 -0800

    change: use format in place of f-strings and use comment style type annotations (#10)

--- src/sagemaker_training/smdataparallel.py
+++ src/sagemaker_training/smdataparallel.py
@@ -197,7 +197,7 @@ class SMDataParallelRunner(process.ProcessRunner):
             # homogeneous mode uses 16 processes per host; 8 server; 8 worker
             smdataparallel_server_addr = self._master_hostname
             smdataparallel_server_port = 7592
-            host_list = [f"{host}:{num_processes_per_host}" for host in self._hosts]
+            host_list = ["{}:{}".format(host, num_processes_per_host) for host in self._hosts]
             smdataparallel_flag = "SMDATAPARALLEL_USE_HOMOGENEOUS=1"
             command = self._get_mpirun_command(
                 num_hosts,
@@ -287,7 +287,8 @@ def _start_sshd_daemon():  # type: () -> None
     subprocess.Popen([sshd_executable, "-D"])
 
 
-def _can_connect(host: str, port: int = 22) -> bool:
+def _can_connect(host, port=22):
+    # type: (str, int) -> bool
     """Check if the connection to provided ``host`` and ``port`` is possible.
 
        Args:
