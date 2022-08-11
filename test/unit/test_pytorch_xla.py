0db2adf7b6fc5b74b993179d820b8f7acf9a7eef
Loki <lokravi@amazon.com>
2022-08-10 18:04:43 -0700

    Refactoring some tests as integration tests

--- test/unit/test_pytorch_xla.py
+++ test/unit/test_pytorch_xla.py
@@ -214,10 +214,10 @@ class TestPyTorchXLARunner:
                 hosts=cluster,
                 num_gpus=num_gpus,
             )
-            with pytest.raises(ClientError) as err:
+            with pytest.raises(ModuleNotFoundError) as err:
                 runner._check_for_torch_xla()
             assert "mechanism requires PT-XLA" in str(err)
-            with pytest.raises(ClientError) as err:
+            with pytest.raises(ModuleNotFoundError) as err:
                 runner._check_for_sagemaker_integration()
             assert "Unable to find SageMaker integration code" in str(err)
 
