3a9f7ff82d57550efd3f1880d0a57d4eef59a0c5
Satish Pasumarthi <35979860+satishpasumarthi@users.noreply.github.com>
2022-10-20 11:19:31 -0700

    feature: Add torch_distributed support for Trainium instances in SageMaker (#154)
    * feature: Add torch_distributed support for Trainium instances

--- src/sagemaker_training/environment.py
+++ src/sagemaker_training/environment.py
@@ -312,6 +312,7 @@ def num_neurons():  # type: () -> int
         neuron_cores = 0
         for item in j:
             neuron_cores += item.get("nc_count", 0)
+        logger.info(f"Found {neuron_cores} neurons on this instance")
         return neuron_cores
     except OSError:
         logger.info("No Neurons detected (normal if no neurons installed)")
@@ -856,6 +857,9 @@ class Environment(mapping.MappingMixin):  # pylint:disable=too-many-public-metho
             )
             or self._additional_framework_parameters.get(
                 "sagemaker_multi_worker_mirrored_strategy_enabled", False
+            )
+            or self._additional_framework_parameters.get(
+                "sagemaker_torch_distributed_enabled", False
             )
         ):
             distribution_instance_groups.append(current_instance_group)
