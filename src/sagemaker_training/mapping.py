4cf4ea5b35b032e5bb172b6008c75adcb4dede19
Vaidotas Simkus <simkus.vaidotas@gmail.com>
2024-09-09 18:07:24 +0300

    fix: maintain hyperparameter order when invoking jobs

--- src/sagemaker_training/mapping.py
+++ src/sagemaker_training/mapping.py
@@ -75,7 +75,7 @@ def to_cmd_args(mapping):  # type: (dict) -> list
         (list): List of cmd arguments.
     """
 
-    sorted_keys = sorted(mapping.keys())
+    mapping_keys = mapping.keys()
 
     def arg_name(obj):
         string = _decode(obj)
@@ -84,15 +84,15 @@ def to_cmd_args(mapping):  # type: (dict) -> list
         else:
             return ""
 
-    arg_names = [arg_name(argument) for argument in sorted_keys]
+    arg_names = [arg_name(argument) for argument in mapping_keys]
 
     def arg_value(value):
         if hasattr(value, "items"):
-            map_items = ["%s=%s" % (k, v) for k, v in sorted(value.items())]
+            map_items = ["%s=%s" % (k, v) for k, v in value.items()]
             return ",".join(map_items)
         return _decode(value)
 
-    arg_values = [arg_value(mapping[key]) for key in sorted_keys]
+    arg_values = [arg_value(mapping[key]) for key in mapping_keys]
 
     items = zip(arg_names, arg_values)
 
